5007dd4f9a167f497fd5a300d0be65810d71933c
kitaev.chen <kitaev@126.com>
2020-07-21 07:56:56 -0700

    tv

---
+++ mycontent/lab1/ex1.py
@@ -0,0 +1,391 @@
+# %% [markdown]
+# ![image](resources/qgss-header.png)
+
+# %% [markdown]
+# # Lab 1: Single-qubit and multi-qubit states, quantum teleportation
+
+# %% [markdown]
+# In this lab, you will learn how to write `Qiskit` code and investigate single-qubit and multi-qubit states using the `qpshere` visualization that you learned in lecture 1.
+#
+# If you have not used Jupyter notebooks before, take a look at the following video to quickly get started.
+# - https://www.youtube.com/watch?v=jZ952vChhuI
+#
+# Remember, to run a cell in Jupyter notebooks, you press `Shift` + `Return/Enter` on your keyboard.
+
+# %% [markdown]
+# ### Installing necessary packages
+
+# %% [markdown]
+# Before we begin, you will need to install some prerequisites into your environment. Run the cell below to complete these installations. At the end, the cell outputs will be cleared.
+
+# %%
+# # !pip install -U -r grading_tools/requirements.txt
+
+from IPython.display import clear_output
+clear_output()
+
+# %% [markdown]
+# # Single-qubit states
+
+# %% [markdown]
+# In lecture, you learned that single qubit states can be written down generally as 
+#
+# $$\sqrt{1-p}\vert0\rangle + e^{i\phi}\sqrt{p}\vert1\rangle$$
+#
+# Here, $p$ is the probability that a measurement of the state in the computational basis $\{\vert0\rangle, \vert1\rangle\}$ will have the outcome $1$, and $\phi$ is the phase between the two computational basis states. 
+#
+# Single-qubit gates can then be used to manipulate this quantum state by changing either $p$, $\phi$, or both.
+#
+# Let's begin by creating a single-qubit quantum circuit. We can do this in `Qiskit` using the following:
+
+# %%
+from qiskit import QuantumCircuit
+
+mycircuit = QuantumCircuit(1)
+mycircuit.draw('mpl')
+
+# %% [markdown]
+# The above quantum circuit does not contain any gates. Therefore, if you start in any state, say $\vert0\rangle$, applying this circuit to your state doesn't change the state. 
+#
+# To see this clearly, let's create the statevector $\vert0\rangle$. In `Qiskit`, you can do this using the following:
+
+# %%
+from qiskit.quantum_info import Statevector
+
+sv = Statevector.from_label('0')
+
+# %% [markdown]
+# You can see what's contained in the object `sv`:
+
+# %%
+sv
+
+# %% [markdown]
+# The vector itself can be found by writing
+
+# %%
+sv.data
+
+# %% [markdown]
+# As you can see, the above matches what you learned in lecture. Recall that $$\vert0\rangle = \begin{bmatrix}1\\0\end{bmatrix}$$
+#
+# We can now apply the quantum circuit `mycircuit` to this state by using the following:
+
+# %%
+new_sv = sv.evolve(mycircuit)
+
+# %% [markdown]
+# Once again, you can look at the new statevector by writing
+
+# %%
+new_sv
+
+# %% [markdown]
+# As you can see, the statevector hasn't changed. Recall the concept of state projection that you learned in lecture. You can compute the projection of `new_sv` onto `sv` by writing
+
+# %%
+from qiskit.quantum_info import state_fidelity
+
+state_fidelity(sv, new_sv)
+
+# %% [markdown]
+# As you can see, the projection of `new_sv` onto `sv` is 1, indicating that the two states are identical. You can visualize this state using the `qsphere` by writing
+
+# %%
+from qiskit.visualization import plot_state_qsphere
+
+plot_state_qsphere(sv.data)
+
+# %% [markdown]
+# As you learned in lecture 1, applying an $X$ gate flips the qubit from the state $\vert0\rangle$ to the state $\vert1\rangle$. To see this clearly, we will first create a single-qubit quantum circuit with the $X$ gate.
+
+# %%
+mycircuit = QuantumCircuit(1)
+mycircuit.x(0)
+
+mycircuit.draw('mpl')
+
+# %% [markdown]
+# Now, we can apply this circuit onto our state by writing
+
+# %%
+sv = Statevector.from_label('0')
+new_sv = sv.evolve(mycircuit)
+new_sv
+
+# %% [markdown]
+# As you can see, the statevector now corresponds to that of the state $\vert1\rangle$. Recall that
+#
+# $$\vert1\rangle = \begin{bmatrix}0\\1\end{bmatrix}$$
+
+# %% [markdown]
+# Now, the projection of `new_sv` onto `sv` is 
+
+# %%
+state_fidelity(new_sv, sv)
+
+# %% [markdown]
+# This is not surprising. Recall from the lecture that the states $\vert0\rangle$ and $\vert1\rangle$ are orthogonal. Therefore, $\langle0\vert1\rangle = 0$. The state can be shown on the `qsphere` by writing
+
+# %%
+plot_state_qsphere(new_sv.data)
+
+# %% [markdown]
+# Similarly, we can create the state $$\frac{1}{\sqrt{2}}\left(\vert0\rangle + \vert1\rangle\right)$$
+# by applying a Hadamard gate as you learned in lecture. Here is how we can create the state and visualize it in `Qiskit`:
+
+# %%
+sv = Statevector.from_label('0')
+mycircuit = QuantumCircuit(1)
+mycircuit.h(0)
+mycircuit.draw('mpl')
+
+# %%
+new_sv = sv.evolve(mycircuit)
+print(new_sv)
+plot_state_qsphere(new_sv.data)
+
+# %% [markdown]
+# As you can see above, the state has equal components of $\vert0\rangle$ and $\vert1\rangle$. The size of the circle is proportional to the probability of measuring each basis state in the statevector. As a result, you can see that the size of the circles is half of the size of the circles in our previous visualizations.
+
+# %% [markdown]
+# Recall from lecture that we can also create other superpositions with different phase. Let's create $$\frac{1}{\sqrt{2}}\left(\vert0\rangle - \vert1\rangle\right)$$ which can be done by applying the Hadamard gate on the state $\vert1\rangle$.
+
+# %%
+sv = Statevector.from_label('1')
+mycircuit = QuantumCircuit(1)
+mycircuit.h(0)
+
+new_sv = sv.evolve(mycircuit)
+print(new_sv)
+plot_state_qsphere(new_sv.data)
+
+# %% [markdown]
+# This time, the bottom circle, corresponding to the basis state $\vert1\rangle$ has a different color corresponding to the phase of $\phi = \pi$. This is because the coefficient of $\vert1\rangle$ in the state $$\frac{1}{\sqrt{2}}\left(\vert0\rangle - \vert1\rangle\right)$$ is $-1$, which is equal to $e^{i\pi}$.
+#
+# Other phases can also be created by applying different gates. The $T$ and $S$ gates apply phases of $+\pi/4$ and $+\pi/2$, respectively. The widget below helps you see different gates, and their actions on single-qubit quantum states.
+
+# %%
+from resources.qiskit_textbook.widgets import gate_demo
+gate_demo(qsphere=True)
+
+# %% [markdown]
+# A summary of the operations of the most common gates on single-qubit states is given by the handy image below, where the phases are shown in degrees.
+#
+# ![image](resources/gates-and-qspheres.png)
+
+# %% [markdown]
+# # Multi-qubit states
+
+# %% [markdown]
+# Similar to the discussion above, you can also explore multi-qubit gates in `Qiskit`. In lecture, you learned about Bell states, and how they can be generated using quantum gates. We will demonstrate below how to create the Bell state $$\frac{1}{\sqrt{2}}\left(\vert00\rangle + \vert11\rangle\right)$$ from the state $\vert00\rangle$. We'll start by visualizing the state $\vert00\rangle$ using the same procedure:
+
+# %%
+sv = Statevector.from_label('00')
+plot_state_qsphere(sv.data)
+
+# %% [markdown]
+# Next, we use the Hadamard gate described above, along with a controlled-X gate, to create the Bell state.
+
+# %%
+mycircuit = QuantumCircuit(2)
+mycircuit.h(0)
+mycircuit.cx(0,1)
+mycircuit.draw('mpl')
+
+# %% [markdown]
+# The result of this quantum circuit on the state $\vert00\rangle$ is found by writing
+
+# %%
+new_sv = sv.evolve(mycircuit)
+print(new_sv)
+plot_state_qsphere(new_sv.data)
+
+# %% [markdown]
+# Note how this looks very similar to a single-qubit superposition with zero phase. Following entanglement, it is no longer possible to treat the two qubits individually, and they must be considered to be one system. 
+#
+# To see this clearly, we can see what would happen if we measured the Bell state above 1000 times.
+
+# %%
+counts = new_sv.sample_counts(shots=1000)
+
+from qiskit.visualization import plot_histogram
+plot_histogram(counts)
+
+# %% [markdown]
+# As you can see above, all measurements give either the result `00` or `11`. In other words, if the measurement outcome for one of the qubits is known, then the outcome for the other is fully determined.
+
+# %% [markdown]
+# ### Ungraded exercise 1
+#
+# Can you create the state $$\frac{1}{\sqrt{2}}\left(\vert01\rangle + \vert10\rangle\right)$$ using a similar procedure?
+
+# %% [markdown]
+# ### Ungraded exercise 2
+#
+# Can you create the state $$\frac{1}{\sqrt{2}}\left(\vert01\rangle - \vert10\rangle\right)$$ using a similar procedure?
+
+# %% [markdown]
+# # Measurements
+
+# %% [markdown]
+# In the above example, we simulated the action of a measurement by sampling counts from the statevector. A measurement can explicitly be inserted into a quantum circuit as well. Here is an example that creates the same Bell state and applies a measurement.
+
+# %%
+mycircuit = QuantumCircuit(2, 2)
+mycircuit.h(0)
+mycircuit.cx(0,1)
+mycircuit.measure([0,1], [0,1])
+mycircuit.draw('mpl')
+# mycircuit.draw()
+
+# %% [markdown]
+# Two new features appeared in the circuit compared to our previous examples. 
+#
+# - First, note that we used a second argument in the `QuantumCircuit(2,2)` command. The second argument says that we will be creating a quantum circuit that contains two qubits (the first argument), and two classical bits (the second argument).
+# - Second, note that the `measure` command takes two arguments. The first argument is the set of qubits that will be measured. The second is the set of classical bits onto which the outcomes from the measurements of the qubits will be stored.
+
+# %% [markdown]
+# Since the above quantum circuit contains non-unitaries (the measurement gates), we will use `Qiskit`'s built-in `Aer` simulators to run the circuit. To get the measurement counts, we can use the following code:
+
+# %%
+from qiskit import Aer, execute
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(mycircuit, simulator, shots=10000).result()
+counts = result.get_counts(mycircuit)
+plot_histogram(counts)
+
+
+# %% [markdown]
+# As you can see, the measurement outcomes are similar to when we sampled counts from the statevector itself.
+
+# %% [markdown]
+# # Graded exercise 1: Quantum teleportation
+
+# %% [markdown]
+# In this graded exercise, you will teleport the quantum state 
+# $$\sqrt{0.70}\vert0\rangle + \sqrt{0.30}\vert1\rangle$$ from Alice's qubit to Bob's qubit. Recall that the teleportation algorithm consists of four major components:
+#
+# 1. Initializing the state to be teleported. We will do this on Alice's qubit `q0`.
+# 2. Creating entanglement between two qubits. We will use qubits `q1` and `q2` for this. Recall that Alice owns `q1`, and Bob owns `q2`.
+# 3. Applying a Bell measurement on Alice's qubits `q0` and `q1`.
+# 4. Applying classically controlled operations on Bob's qubit `q2` depending on the outcomes of the Bell measurement on Alice's qubits.
+#
+# This exercise guides you through each of these steps.
+
+# %% [markdown]
+# ### Initializing the state to be teleported
+
+# %% [markdown]
+# First, create a quantum circuit that creates the state $$\sqrt{0.70}\vert0\rangle + \sqrt{0.30}\vert1\rangle$$ You can do this by using `Qiskit`'s `initialize` function.
+
+# %%
+def initialize_qubit(given_circuit, qubit_index):
+    
+    import numpy as np
+    ### WRITE YOUR CODE BETWEEN THESE LINES - START
+    mycircuit = QuantumCircuit(2, 2)
+    mycircuit.h(0)
+    mycircuit.cx(0,1)
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - END
+    
+    return given_circuit
+
+
+# %% [markdown]
+# Next, we need to create entanglement between Alice's and Bob's qubits.
+
+# %%
+def entangle_qubits(given_circuit, qubit_Alice, qubit_Bob):
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - END
+    
+    return given_circuit
+
+
+# %% [markdown]
+# Next, we need to do a Bell measurement of Alice's qubits.
+
+# %%
+def bell_meas_Alice_qubits(given_circuit, qubit1_Alice, qubit2_Alice, clbit1_Alice, clbit2_Alice):
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+
+    ### WRITE YOUR CODE BETWEEN THESE LINES - END
+
+    
+    return given_circuit
+
+
+# %% [markdown]
+# Finally, we apply controlled operations on Bob's qubit. Recall that the controlled operations are applied in this order:
+#
+# - an $X$ gate is applied on Bob's qubit if the measurement coutcome of Alice's second qubit, `clbit2_Alice`, is `1`.
+# - a $Z$ gate is applied on Bob's qubit if the measurement coutcome of Alice's first qubit, `clbit1_Alice`, is `1`.
+
+# %%
+def controlled_ops_Bob_qubit(given_circuit, qubit_Bob, clbit1_Alice, clbit2_Alice):
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - START
+    
+    
+    ### WRITE YOUR CODE BETWEEN THESE LINES - END
+    
+    return given_circuit
+
+# %% [markdown]
+# The next lines of code put everything together. **You do not need to modify anything below, but you will need to run the cell to submit your solution.**
+
+# %%
+### imports
+from qiskit import QuantumRegister, ClassicalRegister
+
+### set up the qubits and classical bits
+all_qubits_Alice = QuantumRegister(2)
+all_qubits_Bob = QuantumRegister(1)
+creg1_Alice = ClassicalRegister(1)
+creg2_Alice = ClassicalRegister(1)
+
+### quantum teleportation circuit here
+# Initialize
+mycircuit = QuantumCircuit(all_qubits_Alice, all_qubits_Bob, creg1_Alice, creg2_Alice)
+initialize_qubit(mycircuit, 0)
+mycircuit.barrier()
+# Entangle
+entangle_qubits(mycircuit, 1, 2)
+mycircuit.barrier()
+# Do a Bell measurement
+bell_meas_Alice_qubits(mycircuit, all_qubits_Alice[0], all_qubits_Alice[1], creg1_Alice, creg2_Alice)
+mycircuit.barrier()
+# Apply classically controlled quantum gates
+controlled_ops_Bob_qubit(mycircuit, all_qubits_Bob[0], creg1_Alice, creg2_Alice)
+
+### Look at the complete circuit
+print(mycircuit.draw(output='text'))
+
+### store the circuit as the submitted answer
+answer = mycircuit
+
+# %% [markdown]
+# Then, grade your solution by running the cell below. Provide always the same name and email, as the one you wrote during the course sign up.
+
+# %%
+name = 'First Last'
+email = 'first.last@domain.com'
+
+from grading_tools import grade
+grade(answer, name, email, 'lab1', 'ex1')
+
+# %% [markdown]
+# # Additional reading
+#
+# - You can watch a video on building the quantum teleportation quantum circuit here: https://www.youtube.com/watch?v=mMwovHK2NrE&list=PLOFEBzvs-Vvp2xg9-POLJhQwtVktlYGbY&index=6&t=0s
+#
+# - For additional details about the quantum teleportation algorithm, including the principle of deferred measurement, you can refer to the Qiskit Textbook's section on the algorithm here: https://qiskit.org/textbook/ch-algorithms/teleportation.html
+#
+# - The `1 minute Qiskit` episode entitled `What is the qsphere?` succinctly describes the Qsphere visualization tool that we used in this lab. You can find it here: https://youtu.be/4SoK2h4a7us
---
+++ mycontent/preface.md
@@ -0,0 +1,84 @@
+<title>Learn Quantum Computation using Qiskit</title>
+<div class="preface-top">
+    <div class="preface-checker-pattern"></div>
+<div class="preface-summary">
+  <aside class="preface-summary-image"><img src="images/preface_illustration_2.svg"></aside>
+  <div class="preface-summary-text">
+     <p>
+      Greetings from the Qiskit Community team! This textbook is a university quantum algorithms/computation course supplement based on Qiskit to help learn:</p>
+      <ol>
+          <li>The mathematics behind quantum algorithms</li>
+          <li>Details about today's non-fault-tolerant quantum devices</li>
+          <li>Writing code in Qiskit to implement quantum algorithms on IBM's cloud quantum systems</li>
+      </ol>
+</div>
+    <a href="https://qiskit.org/textbook/ch-states/introduction.html"><button class="preface-button read-textbook">Read the textbook <span class="rangle"><img src="/textbook/assets/images/rightarrow.svg"></span></button></a>
+</div>
+
+
+# About the Textbook
+
+<p>This is a free digital textbook that will teach you the concepts of quantum computing while you learn to use the Qiskit SDK.</p>
+
+## Run the Code Inline
+
+<p>This textbook is built on a jupyter notebook framework that allows for easy reading, but it also allows readers to edit and run the code right in the textbook. The chapters can also be opened as Jupyter notebooks in the <a href="https://quantum-computing.ibm.com/jupyter">IBM Quantum Experience</a>, no installs required!</p>
+
+```python
+# Click 'try', then 'run' to see the output,
+# you can change the code and run it again.
+print("This code works!")
+from qiskit import QuantumCircuit
+qc = QuantumCircuit(2) # Create circuit with 2 qubits
+qc.h(0)    # Do H-gate on q0
+qc.cx(0,1) # Do CNOT on q1 controlled by q0
+qc.measure_all()
+qc.draw()
+```
+
+<a href="https://qiskit.org/textbook/widgets-index.html"><button class="preface-button">Interactivity Tour<span class="rangle"><img src="/textbook/assets/images/rightarrow.svg"></span></button></a>
+
+## Learn with Real Quantum Systems
+
+<p>The best way to learn is by doing. Qiskit allows users to run experiments on state-of-the-art quantum devices from the comfort of their homes. The textbook teaches not only theoretical quantum computing, but the experimental quantum physics that realises it.</p>
+
+<img src="images/preface-hw-example.png" class="preface-image">
+
+<a href="https://qiskit.org/textbook/ch-quantum-hardware/accessing_higher_energy_states.html"><button class="preface-button">See Example: Accessing Higher Level States<span class="rangle"><img src="/textbook/assets/images/rightarrow.svg"></span></button></a>
+
+# Using the Textbook
+
+<p>If you're reading the textbook independently, you don't have to read it all in order, but we recommend you read chapters 1-3 first.</p>
+
+## Curriculum Integration
+
+<p>The textbook can be followed as an independent course, however it has been designed to accompany a traditional university course. The textbook shows students how to use Qiskit to experiment with quantum algorithms and hardware, and uses this to reinforce their understanding.
+</p>
+
+<img src="images/curriculum.svg" class="preface-image">
+
+## Use the Textbook in Your Course
+
+If you are using the Qiskit Textbook in your course, you can join the IBM Quantum Educators Program. The Program provides:
+
+<ul class="preface-list">
+<li> The ability to reserve time for priority access to our open systems for in-class demonstrations </li>
+<li> Access to additional premium systems beyond our open systems</li>
+<li> Access to a 5-qubit system with full microwave control using Qiskit Pulse</li>
+</ul>
+    
+<a href="https://quantum-computing.ibm.com/programs/educators"><button class="preface-button">Sign Up for the IBM Quantum Educators Program<span class="rangle"><img src="/textbook/assets/images/rightarrow.svg"></span></button></a>
+
+# Contact
+
+<p> If you have any questions or suggestions about the textbook or would like to incorporate it into your curriculum, please contact Frank Harkins <a href="mailto:Francis.Harkins@ibm.com">(Francis.Harkins@ibm.com)</a>. In the true spirit of open-source, any chapter contributions are welcome in this GitHub repository.</p>
+    
+# Contributors
+
+<p> Learn Quantum Computation using Qiskit is the work of several individuals. If you use it in your work, cite it using <a href="https://github.com/qiskit-community/qiskit-textbook/blob/master/content/qiskit-textbook.bib">this bib file</a> or directly as:</p>
+<p><i>
+Abraham Asfaw, Luciano Bello, Yael Ben-Haim, Sergey Bravyi, Lauren Capelluto, Almudena Carrera Vazquez, Jack Ceroni, Richard Chen, Albert Frisch, Jay Gambetta, Shelly Garion, Leron Gil, Salvador De La Puente Gonzalez, Francis Harkins, Takashi Imamichi, David McKay, Antonio Mezzacapo, Zlatko Minev, Ramis Movassagh, Giacomo Nannicni, Paul Nation, Anna Phan, Marco Pistoia, Arthur Rattew, Joachim Schaefer, Javad Shabani, John Smolin, Kristan Temme, Madeleine Tod, Stephen Wood, James Wootton.</i></p>
+
+```python
+
+```
